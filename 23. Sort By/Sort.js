1fdfb03ab8cedb496c956225543495564b0c22d5
DigvijayDheer <digvijaydheersaini@gmail.com>
2023-08-12 22:11:40 +0530

    add join

--- 23. Sort By/Sort.js
+++ 23. Sort By/Sort.js
@@ -34,3 +34,28 @@ const numbers = [5, 2, 9, 1, 3];
 const sortedBySquares = sortBy(numbers, (num) => num * num);
 console.log(sortedBySquares);
 // Output: [1, 2, 3, 5, 9]
+
+// Example 4:
+const nums = [5, 4, 1, 2, 3];
+// Sorting numbers in ascending order
+const sortedInAscending = sortBy(nums, (x) => x);
+console.log(sortedInAscending);
+// Output: [1, 2, 3, 4, 5];
+
+// Example 5:
+const objArr = [{ x: 1 }, { x: 0 }, { x: -1 }];
+// fn returns the value for the "x" key. So the array is sorted based on that value.
+const sortedObjArr = sortBy(objArr, (d) => d.x);
+console.log(sortedObjArr);
+// Output: [{"x": -1}, {"x": 0}, {"x": 1}]
+
+// Example 6:
+const twoDArr = [
+  [3, 4],
+  [5, 2],
+  [10, 1],
+];
+// arr is sorted in ascending order by number at index=1.
+const sortedTwoDArray = sortBy(twoDArr, (x) => x[1]);
+console.log(sortedTwoDArray);
+// Output: [[10, 1], [5, 2], [3, 4]]
---
+++ 24. Join Two Arrays by ID/Join.js
@@ -0,0 +1,92 @@
+/**===================================================================== */
+// CODE
+/**===================================================================== */
+
+var join = function (arr1, arr2) {
+  arr1.sort((a, b) => a.id - b.id);
+  arr2.sort((a, b) => a.id - b.id);
+  let i = 0,
+    j = 0;
+  const joinedArray = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i].id === arr2[j].id) {
+      joinedArray.push({ ...arr1[i], ...arr2[j] });
+      i++;
+      j++;
+      continue;
+    }
+
+    if (arr1[i].id < arr2[j].id) {
+      joinedArray.push({ ...arr1[i] });
+      i++;
+      continue;
+    }
+    joinedArray.push({ ...arr2[j] });
+    j++;
+  }
+
+  while (i < arr1.length) {
+    joinedArray.push({ ...arr1[i] });
+    i++;
+  }
+
+  while (j < arr2.length) {
+    joinedArray.push({ ...arr2[j] });
+    j++;
+  }
+
+  return joinedArray;
+};
+
+/**===================================================================== */
+// SAMPLE TEST CASES
+/**===================================================================== */
+
+// Example 1:
+const arr1 = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+const arr2 = [
+  { id: 1, age: 25 },
+  { id: 2, age: 30 },
+  { id: 4, age: 28 },
+];
+
+console.log(join(arr1, arr2));
+
+/**
+ * Output:
+ * [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie' },
+    { id: 4, age: 28 }
+   ]
+ */
+
+// Example 2:
+const item1 = [
+  { id: 2, name: "Bob" },
+  { id: 4, name: "David" },
+];
+
+const item2 = [
+  { id: 1, age: 25 },
+  { id: 3, age: 30 },
+  { id: 4, age: 28 },
+];
+
+console.log(join(item1, item2));
+
+/**
+ * Output:
+[
+  { id: 1, age: 25 },
+  { id: 2, name: 'Bob' },
+  { id: 3, age: 30 },
+  { id: 4, name: 'David', age: 28 }
+]
+ */
